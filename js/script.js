879d37de85207e2bab869606328f6b3e2371369c
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-30 16:25:10 +0200

    IF i could find a solution

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log("JSOK");
+let FizzBuzz = "FizzBuzz";
+let Fizz = "Fizz";
+let Buzz = "Buzz";
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+if (i / 3 && i / 5) {
+  i = FizzBuzz;
+  console.log(FizzBuzz);
+} else if (1 / 3) {
+  i = Fizz;
+} else if (i / 5) {
+  i = Buzz;
+}
